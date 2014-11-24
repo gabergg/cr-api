c75113ad7a9d5123d3eae1363912cfdb50603b24
Gabe G'Sell <gabergg@gmail.com>
2014-11-23 21:09:57 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrApi::Application.config.secret_key_base = 'e5dbddeea4e092ba27e8c1c2d7fe32adac47a8e304ee4e17c6e88b7eacead3d6f93c7032adb2d46f141681ad49dc343907383f2b969a7fde184fdc0253f1aa8e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CrApi::Application.config.session_store :cookie_store, key: '_cr-api_session'
